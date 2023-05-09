3483c99e7f8d79b12a818bc6ba7a9d6459326856
Wellfix <dbrozyna15@gmail.com>
2023-05-09 13:27:45 +0200

    feat: updated meta tags for apple

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,7 +7,14 @@ export default function App({Component, pageProps}) {
             <Head>
                 <meta charSet="UTF-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <meta name="apple-mobile-web-app-capable" content="yes"/>
                 <link rel="manifest" href="/manifest.json"/>
+                <link rel="apple-touch-icon" href="/logo.png" />
+                <link rel="apple-touch-icon" sizes="152x152" href="/logo.png" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/logo.png" />
+                <link rel="apple-touch-icon" sizes="167x167" href="/logo.png" />
+                    
+                    
             </Head>
             <Component {...pageProps} />
         </>
