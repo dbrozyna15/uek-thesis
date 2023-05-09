e4acedc963d7c8af639ab83867fcbb38439c3bcc
Wellfix <dbrozyna15@gmail.com>
2023-05-09 13:17:20 +0200

    feat: modified manifest.json, added next-pwa

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import Head from "next/head";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}) {
+    return (
+        <>
+            <Head>
+                <meta charSet="UTF-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link rel="manifest" href="/manifest.json"/>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
